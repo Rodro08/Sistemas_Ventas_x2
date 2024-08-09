32484351ec6e8539fddf25345747041f56999e0c
Rodro08 <roodrigoaraque1002@gmail.com>
2024-08-09 00:38:23 -0600

    Avance hasta video 9 en su totalidad

--- ventas/src/main/java/CapaLogica/N_Producto.java
+++ ventas/src/main/java/CapaLogica/N_Producto.java
@@ -4,8 +4,12 @@
  */
 package CapaLogica;
 
+
+import CapaDatos.M_Producto;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -25,7 +29,7 @@ public class N_Producto {
         modelo=new DefaultTableModel(null,titulos);
         String[] registro =new String[10];
         
-        sql=("exec sp_buscar_cliente '"+ buscar+"'");
+        sql=("exec sp_buscar_producto'"+ buscar+"'");
         try{
             Statement st =cn.createStatement();
             ResultSet rs =st.executeQuery(sql);
@@ -40,12 +44,138 @@ public class N_Producto {
                 registro[6]=rs.getString(7);
                 registro[7]=rs.getString(8);
                 registro[8]=rs.getString(9);
+                registro[9]=rs.getString(10);
                 modelo.addRow(registro);
             }
             return modelo;
         }catch(Exception e){
             JOptionPane.showConfirmDialog(null, e);
             return null;
+        }
+    }
+    public boolean insertar(M_Producto dts) throws SQLException{
+        sql=("{call sp_guardar_producto (?,?,?,?,?,?,?,?)}");
+        try{
+             PreparedStatement pst =cn.prepareStatement(sql);
+            pst.setString(1, dts.getIdproducto());
+            pst.setString(2, dts.getSerie());
+            pst.setString(3, dts.getNombre());
+            pst.setDate(4, dts.getF_ingreso());
+            pst.setDate(5, dts.getF_vencimiento());
+            pst.setDouble(6, dts.getP_venta());
+            pst.setDouble(7, dts.getP_compra());
+            pst.setInt(8, dts.getIdcategoria());
+            
+            int n= pst.executeUpdate();
+            
+            if(n != 0){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e);
+            return false;
+        }
+    }
+    
+    public boolean editar(M_Producto dts) throws SQLException{
+        sql=("{call sp_editar_producto (?,?,?,?,?,?,?,?)}");
+        try{
+             PreparedStatement pst =cn.prepareStatement(sql);
+            pst.setString(1, dts.getIdproducto());
+            pst.setString(2, dts.getSerie());
+            pst.setString(3, dts.getNombre());
+            pst.setDate(4, dts.getF_ingreso());
+            pst.setDate(5, dts.getF_vencimiento());
+            pst.setDouble(6, dts.getP_venta());
+            pst.setDouble(7, dts.getP_compra());
+            pst.setInt(8, dts.getIdcategoria());
+            
+            int n= pst.executeUpdate();
+            
+            if(n != 0){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e);
+            return false;
+        }
+    }
+    
+    public boolean eliminar(M_Producto dts){
+          sql=("{call sp_eliminar_producto(?)}");
+          
+          try {
+              PreparedStatement pst = cn.prepareStatement(sql);
+              pst.setString(1, dts.getIdproducto());
+              
+              int n = pst.executeUpdate();
+              if(n != 0){
+                return true;
+            }else{
+                return false;
+            }
+          }catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e);
+            return false;
+        }
+                  
+      }
+    
+    public int gernerarIdproducto(){
+          String sql=("select max(idproducto) as id from producto");
+          int cod =0;
+          
+          try{
+              Statement st =cn.createStatement();
+              ResultSet rs=st.executeQuery(sql);
+              
+              if(rs.next()){
+                  cod=rs.getInt("id")+1;
+              }
+          }catch(SQLException e1){
+                   JOptionPane.showConfirmDialog(null,"error sql:"+ e1.getMessage());
+          }
+          return cod;
+      }
+    
+    public boolean disminuir(int idproducto, int cantidad){
+        sql="update productos set cantidad=cantidad-?"+
+                "where idproducto=?";
+        try{
+            PreparedStatement pst=cn.prepareStatement(sql);
+            pst.setInt(1, cantidad);
+            pst.setInt(2, idproducto);
+            int n =pst.executeUpdate();
+            if (n!=0){
+                return true;
+            }else{
+                return false;
+            }                      
+        }catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e);
+            return false;
+        }
+    }
+    public boolean aumentar(int idproducto, int cantidad){
+        sql="update productos set cantidad=cantidad+?"+
+                "where idproducto=?";
+        try{
+            PreparedStatement pst=cn.prepareStatement(sql);
+            pst.setInt(1, cantidad);
+            pst.setInt(2, idproducto);
+            int n =pst.executeUpdate();
+            if (n!=0){
+                return true;
+            }else{
+                return false;
+            }                      
+        }catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e);
+            return false;
         }
     }
 }
